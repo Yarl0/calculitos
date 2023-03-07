38971dad6b38830653f624bc31ee9c636b8220f6
STX <jleon540@unab.edu.co>
2023-03-07 03:31:47 -0500

    Codigo iniciado

---
+++ app/src/main/java/com/example/calculitos/MainActivity.java
@@ -0,0 +1,20 @@
+package com.example.calculitos;
+
+import androidx.appcompat.app.AppCompatActivity;
+
+import android.annotation.SuppressLint;
+import android.os.Bundle;
+import android.widget.TextView;
+
+public class MainActivity extends AppCompatActivity {
+
+    private TextView answ;
+
+    @SuppressLint("WrongViewCast")
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_main);
+
+        answ=findViewById(R.id.answ);
+    }
+}
